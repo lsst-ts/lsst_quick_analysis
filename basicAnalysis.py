21103f8fd855df6eca94fb2f358371ce79b5bfd5
Andrew Heyer <AHeyer@lsst.org>
2017-08-28 11:27:57 -0700

    basic framework set up

---
+++ basicAnalysis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# This is my first script.
+
+import sqlite3
+import database
+
+class basicAnalysis:
+
+	def visitePerNight(self):
+		print("Calculating Visits Per Night...")
+
+
+
+ba = basicAnalysis()
+ba.visitePerNight()
---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+class database:
+
+	path = '/Users/aheyer/Projects/mafDevelopment/test1/colossus_2177.db'
+	conn = sqlite3.connect(path)
+
+	def connect(self):
+		c = self.conn.cursor()	
+		return curs

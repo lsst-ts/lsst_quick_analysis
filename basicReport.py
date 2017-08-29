234b2ac5826ae1bacfc3dd19a7a7cc6e4fe2d286
Andrew Heyer <AHeyer@lsst.org>
2017-08-29 16:15:32 -0700

    reporting file writer complete

--- basicReport.py
+++ basicReport.py
@@ -14,6 +14,10 @@ class basicReport:
 	ERASE_LINE = '\x1b[2K'
 	directory = "reports/"
 
+	# Printing "parts"
+
+	LINE = "+-----------+------------------+------------------+------------+------------+------------+-------------+----------------+-------------+"
+	HEADER = "| slewCount | startDate        | endDate          | slewTime   |  slewDist  | startTelAz | startTelAlt | startRotTelPos | startFilter |"
 
 	if not os.path.exists(directory):
 		os.makedirs(directory)
@@ -21,18 +25,39 @@ class basicReport:
 
 	def writeOutBasicInfo(self):
 
-		# file = open("reports/test.txt","w") 
+		file = open("reports/test.txt","w") 
+
+		file.write(self.LINE + "\n")
+		file.write(self.HEADER + "\n")
+		file.write(self.LINE + "\n")
+
 
 		count = 0
 
-		# for row in self.c.execute("SELECT SlewHistory.slewCount, SlewHistory.startDate, SlewHistory.endDate, SlewHistory.slewTime, SlewHistory.slewDistance, SlewInitialState.telAz AS startTelAz, SlewInitialState.telAlt AS startTelAlt, SlewInitialState.rotTelPos AS startRotTelPos, SlewInitialState.filter AS startFilter from SlewHistory, SlewInitialState"):
-		for row in self.c.execute("SELECT SlewHistory.slewCount, SlewHistory.startDate, SlewHistory.endDate, SlewHistory.slewTime, SlewHistory.slewDistance, SlewInitialState.telAz AS startTelAz, SlewInitialState.telAlt AS startTelAlt, SlewInitialState.rotTelPos AS startRotTelPos, SlewInitialState.filter AS startFilter from SlewHistory JOIN SlewInitialState ON SlewHistory.slewCount = SlewInitialState.SlewHistory_slewCount JOIN SlewMaxSpeeds ON SlewInitialState.SlewHistory_slewCount = SlewMaxSpeeds.SlewHistory_slewCount"):
+		for row in self.c.execute("""SELECT SlewHistory.slewCount, 
+											SlewHistory.startDate, 	
+											SlewHistory.endDate, 
+											SlewHistory.slewTime, 
+											SlewHistory.slewDistance, 
+											SlewInitialState.telAz AS startTelAz, 
+											SlewInitialState.telAlt AS startTelAlt, 
+											SlewInitialState.rotTelPos AS startRotTelPos, 
+											SlewInitialState.filter AS startFilter FROM SlewHistory 
+											JOIN SlewInitialState ON SlewHistory.slewCount = SlewInitialState.SlewHistory_slewCount"""):
+			
+			file.write('|{:>10} | {:>16} | {:>16} | {:>10} | {:>10} | {:>10} | {:>11} | {:>14} | {:>11} |'
+				.format(str(row[0]), str(row[1]), str(row[2]), str(round(row[3],6)), str(round(row[4],6)), str(round(row[5],6)), str(round(row[6],6)), str(round(row[7],6)), str(row[8]) ) )
+
+
+			file.write("\n") 
+
+
+
+
 			if count == 10:
 				break
 			count += 1
-			print(row)
-
-
+			
 
 br = basicReport()
 

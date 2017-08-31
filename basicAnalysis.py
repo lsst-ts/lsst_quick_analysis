16a5a8abe0e98fe096b6044f9e24c60c320680f4
Andrew Heyer <AHeyer@lsst.org>
2017-08-31 15:30:26 -0700

    Refractor and added some more metrics

--- basicAnalysis.py
+++ basicAnalysis.py
@@ -7,96 +7,151 @@ import sqlite3
 from database import *
 from config import *
 
+"""I wanted to make the basicAnalysis class as decoupled as possible. Every
+definition will be it's own metric. There may be redundency when calculating 
+averages for example. However a user will be free to comment out any metrics
+they wish with no recoil. For ease of readbility, variables should be very
+explicit. 
+"""
 class basicAnalysis:
 
 	db = database()
 	c = db.connect()
-	CURSOR_UP_ONE = '\x1b[1A'
-	ERASE_LINE = '\x1b[2K'
+
+	
+	# Our dictionaries that are used for all metric calculations
+	obs_history_table = {"night":[], "filter":[]}
+	slew_history_table = {"slewTime":[]}
+
+	"""Any columns that need to be used for the analysis should be loaded here.
+	Once loaded it is much faster to do our analysis on a python array than it
+	is to run more query's.
+	"""
+	def __init__(self):
+
+		for row in self.c.execute("SELECT night, filter FROM ObsHistory;"):	
+
+			self.obs_history_table["night"].append(row[0])
+			self.obs_history_table["filter"].append(row[1])
 
 
-	def nightNumbers(self):
-		
-		print("Calculating night numbers...")
-
-		exposureCount = 0
-		visitsPerNightCounter = {}
-
-
-		# Get the total amount of exposures, as well as how many nights these exposure count for
-		for row in self.c.execute("SELECT night FROM ObsHistory;"):			
+		for row in self.c.execute("SELECT slewTime FROM SlewHistory;"):
 			
-			exposureCount+=1
-			currNight = row[0]
+			self.slew_history_table["slewTime"].append(row[0])
 
-			if currNight in visitsPerNightCounter:
-				visitsPerNightCounter[currNight] = visitsPerNightCounter[currNight] + 1
-			else:
-				visitsPerNightCounter[currNight] = 1
-
-		totalVisitCount = int(exposureCount/VISIT)
-
-		print("		number of exposures: " + str(exposureCount))
-		print("		number of visits: " + str(totalVisitCount))
-		print("		number of nights: " + str(len(visitsPerNightCounter)))
-		print("		visits/night avg: " + str(round(totalVisitCount/(len(visitsPerNightCounter)),4)))
+		print("\n" + ("=" * 80) ) 
+		print("CALCULATING METRICS...")
+		print("-" * 80)
 
 
+	def numberOfExposures(self):
 
-	def totalFilterChanges(self):
+		numberOfExposures = len(self.obs_history_table["night"])
 
-		print("Calculating total filter changes... ")
-
-		counter = 0
-		curs = self.c.execute("SELECT filter FROM ObsHistory;")
-		currentFilter = curs.fetchone()[0]
+		print("		number of exposures: " + str(numberOfExposures))
 
 
-		for row in self.c.execute("SELECT filter FROM ObsHistory;"):			
-			
-			if currentFilter == row[0]:
+	def numberOfVisits(self):
+
+		numberOfVisits = int(len(self.obs_history_table["night"])/VISIT)
+
+		print("		number of visits: " + str(numberOfVisits))
+
+	
+	"""By counting the amount of unique values inside of the "night" array that
+	is within the obs_history_table, we can find the amount of nights that this
+	survey covered. It may be intuitve to take the last integer of the array,
+	however this value would be the total amount of nights, including those
+	that the camera was down due to repair/filter changes/ etc.
+	"""
+	def numberOfObservedNights(self):
+
+		numberOfObservedNights = 1
+
+		currentNight = self.obs_history_table["night"][0]
+
+		for night in self.obs_history_table["night"]:
+
+			if night == currentNight:
 				continue
 			else:
-				counter += 1
-				currentFilter = row[0]
+				numberOfObservedNights += 1
+				currentNight = night
 
-		print(self.CURSOR_UP_ONE + self.ERASE_LINE + "Calculating total filter changes... " + str(counter))
+		print("		number of observed nights: " + str(numberOfObservedNights))
 
 
-	def slewTimeNumbers(self):
+	def averageVisitsPerObservedNights(self):
+		
+		numberOfVisits = int(len(self.obs_history_table["night"])/VISIT)
 
-		print("Calculating slew time numbers...")
+		numberOfObservedNights = 1
 
-		# for calculating avg
-		totalSlewTimes = 0
+		currentNight = self.obs_history_table["night"][0]
+
+		for night in self.obs_history_table["night"]:
+
+			if night == currentNight:
+				continue
+			else:
+				numberOfObservedNights += 1
+				currentNight = night
+
+		averageVisitsPerObservedNights = round(numberOfVisits/numberOfObservedNights, 4)
+
+		print("		avg visits/observed nights: " + str(averageVisitsPerObservedNights))
+
+
+	def numberOfFilterChanges(self):
+		
+		filterChangeCounter = 0
+		currentCamFilter = self.obs_history_table["filter"][0]
+
+		for camFilter in self.obs_history_table["filter"]:			
+			
+			if camFilter == currentCamFilter:
+				continue
+			else:
+				filterChangeCounter += 1
+				currentCamFilter = camFilter
+
+		print("		number of filter changes: " + str(filterChangeCounter))
+
+
+	def maxSlewTime(self):
+
+		maxSlewTime = round(max(self.slew_history_table["slewTime"]),4)
+
+		print("		max slew time: " + str(maxSlewTime))
+
+
+	def minSlewTime(self):
+
+		minSlewTime = round(min(self.slew_history_table["slewTime"]),4)
+
+		print("		min slew time: " + str(minSlewTime))
+
+
+	def avgSlewTime(self):
+
 		totalSlewTime = 0
+		slewTimeCount = len(self.slew_history_table["slewTime"])
 
-		# starting val for calculating min & max
-		curs = self.c.execute("SELECT slewTime FROM SlewHistory;")
-		maxVal = curs.fetchone()[0]
-		minVal = maxVal
-
-		for row in self.c.execute("SELECT slewTime FROM SlewHistory;"):			
-			slewTime = row[0]
-			totalSlewTimes += 1
+		for slewTime in self.slew_history_table["slewTime"]:
 			totalSlewTime += slewTime
 
-			if slewTime < minVal:
-				minVal = slewTime
+		avgSlewTime = round(totalSlewTime/slewTimeCount, 4)
 
-			if slewTime > maxVal:
-				maxVal = slewTime
-
-		slewAverage = totalSlewTime/totalSlewTimes
-
-		print("		avg: " + str(slewAverage))
-		print("		min: " + str(minVal))
-		print("		max: " + str(maxVal))
-
-
+		print("		avg slew time: " + str(avgSlewTime))
+		
 
 ba = basicAnalysis()
 
-ba.nightNumbers()
-# ba.totalFilterChanges()
-ba.slewTimeNumbers()
+ba.numberOfExposures()
+ba.numberOfVisits()
+ba.numberOfObservedNights()
+ba.averageVisitsPerObservedNights()
+ba.numberOfFilterChanges()
+ba.maxSlewTime()
+ba.minSlewTime()
+ba.avgSlewTime()
